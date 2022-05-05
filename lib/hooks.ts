fd391b66f98917ceb3b6568c3943cdb4f1209f2f
Adeniyii <ifedayoadeniyi@gmail.com>
2022-05-06 00:19:13 +0100

    implement custom hooks to fetch playlist and user data

---
+++ lib/hooks.ts
@@ -0,0 +1,22 @@
+import useSWR from "swr";
+import fetcher from "./fetcher";
+
+export const useMe = () => {
+  const { data, error } = useSWR("/me", fetcher);
+
+  return {
+    user: data,
+    isError: error,
+    isLoading: !data && !error,
+  };
+};
+
+export const usePlaylist = () => {
+  const { data, error } = useSWR("/playlist", fetcher);
+
+  return {
+    playlists: data || [],
+    isError: error,
+    isLoading: !data && !error,
+  };
+};
